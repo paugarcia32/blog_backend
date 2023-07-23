a836c3e560c1c7008eda3129d79cd8b0ca212534
paugarcia32 <paugarcia32@gmail.com>
2023-07-24 01:30:08 +0200

    some errors

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 const User = require('./models/User')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 require('dotenv').config()
 
 const salt = bcrypt.genSaltSync(10)
@@ -13,31 +14,30 @@ const secret = process.env.SECRET_KEY
 
 app.use(cors({credentials:true, origin:process.env.URL}))
 app.use(express.json())
+app.use(cookieParser())
 
 mongoose.connect(process.env.DB_URI);
 
-app.post('/register', async (req, res) =>{
+app.post('/register', async (req, res) => {
   const {username,password} = req.body;
   try{
     const userDoc = await User.create({
-    username,
-    password:bcrypt.hashSync(password, salt)
-  })
-  res.json(userDoc);
-  }catch(e){
-res.status(400).json(e)
+      username,
+      password:bcrypt.hashSync(password, salt)
+    })
+    res.json(userDoc);
+  } catch(e){
+      res.status(400).json(e)
   }
-
-
 })
 
 
-app.post('/login', async (req, res) =>{
+app.post('/login', async (req, res) => {
   const {username,password} = req.body;
   const userDoc = await User.findOne({username})
 
   if (!userDoc) {
-    return res.status(400).json("wrong credentials"); // Credenciales incorrectas, enviar respuesta de error y detener la función
+    return res.status(400).json("wrong credentials");
   }
 
   const passOK = bcrypt.compareSync(password, userDoc.password)
@@ -51,6 +51,19 @@ app.post('/login', async (req, res) =>{
     res.status(400).json("wrong credentials")
   }
 })
+
+app.get('/profile', (req, res) => {
+  const {token} = req.cookies
+  jwt.verify(token, secret, {}, (err,info) => {
+    if (err) throw err
+    res.json(info)
+  })
+})
+
+app.post('/logout', (req,res) => {
+  res.cookie('token', '').json('ok')
+})
+
 
 app.listen(4000);
 
